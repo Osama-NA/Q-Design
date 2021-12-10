8d52c4e7af6545199fc3069b9c723016dc7c4206
Osama Hajj Ali <ossama.nae12@gmail.com>
2021-12-10 23:59:09 +0200

    contact us form

---
+++ public/js/contact.js
@@ -0,0 +1,40 @@
+// VARIABLES
+const selectedService = document.querySelector('.menu-selected');
+const selectedServiceInput = document.querySelector('.menu-selected input');
+const selectedServiceIcon = document.querySelector('.menu-selected i');
+const servicesMenu = document.querySelector('.menu');
+const services = document.querySelectorAll('.menu li');
+
+let showServicesMenu = true;
+
+
+// FUNCTIONS
+const handleSelectedServiceClick = () => {
+    servicesMenu.style.display = showServicesMenu ? 'flex' : 'none';
+    selectedServiceIcon.style.transform = showServicesMenu ? 'rotate(180deg)' : 'rotate(0deg)';
+
+    showServicesMenu = !showServicesMenu;
+}
+
+// sets the selected service to current service and vice versa 
+// to allow user to re-select previously selected service
+// handleSelectedServiceClick is called to close menu after a service is selected
+const setChosenService = (e) => {
+    let currentService = selectedServiceInput.value;
+    let chosenService = e.target.innerText;
+
+    selectedServiceInput.value = chosenService;
+    e.target.innerText = currentService;
+
+    handleSelectedServiceClick();
+}
+
+
+// EVENT LISTENERS
+selectedService.addEventListener('click', handleSelectedServiceClick);
+
+if(services){
+    for(let i = 0; i < services.length; i++){
+        services[i].addEventListener('click', setChosenService);
+    }
+}
